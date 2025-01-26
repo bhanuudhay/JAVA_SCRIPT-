a2a337cc248c3b60321f3ad36b21a87e5d31be4d
Bhanu Udhay Singh <91338302+bhanuudhay@users.noreply.github.com>
2025-01-26 01:27:10 +0000

    var and datatypes

---
+++ 01_basics/01_var.js
@@ -0,0 +1,19 @@
+const accountId = 2424 // value can't be change now 
+let accountEmail = "bhanu@gmIL.com"
+var accountPassword = "12345"
+accountCity = "Jaipur"
+let accountState
+
+// var aur let dono method variable declare krne ke liya hota hai but problem arises in var that is scope {} agar iska 
+// under kuch change krta tha toh sab jagah  change ho jata tha 
+// accountId = 34  // not allowed
+
+
+accountEmail = "dev@gmail.com"
+accountPassword = "211121"
+accountCity = "banglore"
+
+console.log(accountId);
+
+console.table([accountId , accountEmail , accountPassword , accountCity ,accountState])
+
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,30 @@
+"use strict" ; // treat all js code as newer version
+
+// alert(3+3) //we are using nodejs not browser
+
+// whenever writing code you should maintain code readability
+
+console.log(
+    3+
+    3)
+
+console.log("bhanu")
+
+
+let name  = "bhanu"
+let age = 19
+let isTrue = false
+
+// number => 2^53
+// bigint
+//string=> ""
+//boolean => true | false
+//null standalone value
+//undefined => abhi value assign nhi hui hai
+//symbol  => unique
+
+
+//object 
+
+console.log(typeof null); // object
+console.log(typeof undefined); // undefined
