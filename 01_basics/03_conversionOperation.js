36711e7ef0e0a5993342e5eff8e2307549d5efdf
Bhanu Udhay Singh <91338302+bhanuudhay@users.noreply.github.com>
2025-01-26 02:02:13 +0000

    operations

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -24,4 +24,42 @@ let stringNum = String(number)
 
 console.log(stringNum);
 console.log(typeof stringNum);
+ 
+
+//***********************************operations*********************************************/
+
+let value = 9
+let negval = -value
+
+console.log(negval);
+console.log(2**2); // power 
+
+let str1  = "bhanu"
+let str2  = "udhay"
+
+let str3 = str1+str2
+
+console.log(str3);
+console.log("1" + 2);
+console.log(1+"2");
+console.log("1"+2+2);
+console.log(1+2+"2");
+
+
+console.log((3+2)*3-2); // use brackets
+
+console.log(+true); // 1
+
+console.log(+""); // 0
+
+let num1, num2 , num3
+
+num1 = num2 = num3 = 3+3
+
+let gameCounter  = 100
+// gameCounter++ // 101
+++gameCounter // 101
+console.log(gameCounter);
+
+
 
