318c95bccbc701e5a5868dd2bbe2261035128439
Bhanu Udhay Singh <91338302+bhanuudhay@users.noreply.github.com>
2025-01-26 02:14:51 +0000

    comparison

---
+++ 01_basics/04_comparisons.js
@@ -0,0 +1,24 @@
+// console.log(2 > 3)
+// console.log(2 >= 3)
+// console.log(2 <= 3)
+// console.log(2 < 3)
+// console.log(2 != 3)
+// console.log(2 == 3)
+
+// console.log("2" > 1); true
+// console.log("02" > 1); true
+
+console.log(null > 0);
+console.log(null >= 0);
+console.log(null == 0);
+
+// comaprison and equality works differently  in comparion null=>0
+
+console.log(undefined == 0);
+console.log(undefined >= 0);
+console.log(undefined > 0);
+
+// === strict check
+// value  + datatype bhi check kraga
+
+console.log("02" === "2");
