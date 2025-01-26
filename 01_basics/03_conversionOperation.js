bc5ce7c70f889b18d939fa9ff0c8da7d09a5df57
Bhanu Udhay Singh <91338302+bhanuudhay@users.noreply.github.com>
2025-01-26 01:44:31 +0000

    conversion

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,27 @@
+let score = false
+
+console.log(typeof score);
+
+let valueInNumber  = Number(score)
+
+console.log(typeof valueInNumber);
+
+console.log(valueInNumber); // NaN not a number 
+
+// "33" => 33
+// "33dds" => NaN
+// true => 1 || false => 0
+
+let isLogged =1
+let booleanisLogged = Boolean(isLogged)
+
+console.log(booleanisLogged);
+
+// "" => false ||  "sde" => true
+
+let number = 22
+let stringNum = String(number)
+
+console.log(stringNum);
+console.log(typeof stringNum);
+
