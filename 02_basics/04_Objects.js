bdfa3a3d4356d7f70ea3e5ca6adfb8d6a1d4f9ae
Bhanu Udhay Singh <91338302+bhanuudhay@users.noreply.github.com>
2025-01-26 13:20:46 +0000

    object destructuring and json api

--- 02_basics/04_Objects.js
+++ 02_basics/04_Objects.js
@@ -40,3 +40,31 @@ console.log(Object.values(tinderUser1))
 console.log(Object.entries(tinderUser1))  // object to array 
 
 console.log(tinderUser1.hasOwnProperty('name')) // kya jeh present hai 
+
+// object destructuring 
+
+const course = {
+    courceName : "DSA",
+    price :100,
+    courseInstructor : "Bhanu"
+}
+
+// course.price
+//this is just syntaxixally sugar kra na kra it depend on you 
+
+const {courseInstructor : instructor} = course  // ap name bhi change kr skta ho
+console.log(instructor)
+
+// API : jab bhi apko apna kam kisi aur ke sir pe dalna hai toh api use krta hai 
+
+// {
+//     "name" :"bhanu udhay",
+//     "class" : "3rd year",
+//      "roll-no" : "34"
+// }
+
+// [
+//     {},
+//     {},
+//     {}
+// ]
