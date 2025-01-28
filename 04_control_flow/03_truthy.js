2b2a73d575bbf5c2e6bef1efd0bc051c52778108
Bhanu Udhay Singh <91338302+bhanuudhay@users.noreply.github.com>
2025-01-28 18:54:19 +0000

    Nullish Coalescing operator

--- 04_control_flow/03_truthy.js
+++ 04_control_flow/03_truthy.js
@@ -21,3 +21,26 @@ if(Object.keys(emptyObj).length === 0 )
 console.log(false == 0)
 console.log(false == '')
 console.log(0 == '')
+
+/// NUllish coalescing Operator (??) :: null , undefined  // only about these twos 
+
+let val1
+//val1  = 5 ?? 10
+// val1  = null ?? 10
+// val1 = undefined ?? 15
+val1 = null ?? 30 ?? 20  //  is case meh jo bhi first value hogi woh assign kar dega 
+
+console.log(val1) // 5
+
+// # basically for safety 
+//  kya faida hua agar 1st value hi jani hai ?
+// ans  : jab app database or firebase se call kroga you dont get res directly apko 2 value milti hai 
+// kya pata apka pass null or undefined in that case full code structure parashan ho jaya 
+
+
+// terniary operator 
+
+// condition ? true : false
+
+const iceTeaPrice =40
+iceTeaPrice <=40 ? console.log("Affordable") :console.log("Mai nhi lega re")
