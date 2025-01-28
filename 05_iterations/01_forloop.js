67254f30c660db7caa586cd563586d2ed1f29fba
Bhanu Udhay Singh <91338302+bhanuudhay@users.noreply.github.com>
2025-01-28 19:17:03 +0000

    forloop

---
+++ 05_iterations/01_forloop.js
@@ -0,0 +1,30 @@
+//for 
+// for(let i = 0 ; i<11 ; i++)
+// {
+//     let el = i;
+//     if(el == 5)
+//     {
+//         console.log("5 is best no")
+//     }
+//     console.log(i + " ")
+// }
+
+// for(let i = 10; i >=1;i--)
+// {
+//     for(let j = 1 ;j<i ;j++)
+//     {
+//         const el = j;
+//         console.log(j)
+//     }
+// }
+
+// let myarr = ["hi" ,"bye","tata"]
+// console.log(myarr.length)
+// for(let i =0 ; i<=myarr.length ;i++)     ///  arrayoutofbound it gives undefined 
+// {
+//     const el = myarr[i]
+//     console.log(el)
+// }
+
+
+// break and continue
