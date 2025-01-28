af919193562529ed39ad7c0a22f84ba31bccbecf
Bhanu Udhay Singh <91338302+bhanuudhay@users.noreply.github.com>
2025-01-28 19:27:28 +0000

    while , do-while

---
+++ 05_iterations/02_whileloops.js
@@ -0,0 +1,21 @@
+// while loop
+// let index = 0
+// while(index <=10)
+// {
+//     console.log(`Value of index is ${index}`)
+//     index = index+2
+// }
+
+// let ind = 0
+// let arr = ["hw","cs","sf","er"]
+// while(ind < arr.length)
+// {
+//     console.log(`value is ${arr[ind]}`)
+//     ind = ind+1
+// }
+
+let score  =11
+do{
+    console.log(`score is ${score}`)
+    score++
+}while(score <10)
