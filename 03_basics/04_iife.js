bd1251e9bbb0743a7296a77cdbeb4f27899c6403
Bhanu Udhay Singh <91338302+bhanuudhay@users.noreply.github.com>
2025-01-28 18:29:58 +0000

    Immediately Invoked Function Expression

--- 03_basics/04_iife.js
+++ 03_basics/04_iife.js
@@ -2,11 +2,11 @@
 /*
 Use cases
 Global Scope ke pollution se problem hoti hai many times so from avoiding that we use this 
-Aur function instantly execute ho jaya
+haiAur function instantly execute ho jaya
 */
 
 
-// iife kaisa likhta hai 
+// iife kaisa likhta  
 // ()() //pehla wala  meh function likhta hai aur dusra wala execute ke liya
 
 (function greet()
