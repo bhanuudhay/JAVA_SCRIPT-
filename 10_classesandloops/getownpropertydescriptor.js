4b251b285b902c5d428fb8e1ab13142be35adda1
Bhanu Udhay Singh <91338302+bhanuudhay@users.noreply.github.com>
2025-02-03 16:31:35 +0000

    bind and propertydescriptor

---
+++ 10_classesandloops/getownpropertydescriptor.js
@@ -0,0 +1,31 @@
+//const descriptor = Object.getOwnPropertyDescriptor(Math , "PI") // object ke baare meh kuch hidden chije btati hai
+//console.log(descriptor)
+// console.log(Math.PI)
+// const pii  = Math.pi
+
+const chai = {
+    name :"masala chai",
+    price : 900,
+    isAvailable : true,
+
+    orderChai : function(){
+        console.log("chai nhi bani")
+    }
+}
+
+console.log(Object.getOwnPropertyDescriptor(chai ,  "name"))  // agar sirf chai likho ge toh error aayaga  kyunki chai toh ek object object hai 
+// toh isliye second parameter dena padta hai 
+Object.defineProperty(chai, 'name', {
+   // writable: false,
+    enumerable: true,
+})
+
+console.log(Object.getOwnPropertyDescriptor(chai ,  "name")) 
+
+for(let [key,value] of Object.entries(chai))  // not iterable likh ke aayaga 
+{
+    if(typeof value !== 'function')
+    console.log(`${key} : ${value}`)
+}
+
+// main point is file ka yeh hai ki ham kisi bhi object ki properties dekh skta hai jo bhi unko inbuild milti hai 
